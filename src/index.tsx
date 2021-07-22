2f4036d7a78f74b3e98bc74ccbe067ee6c43d76f
andrey <guschaninov@yandex.ru>
2021-07-23 01:49:52 +0300

    add tag

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { render } from "react-dom";
 import { App } from "./components/App";
 import { QueryClient, QueryClientProvider } from "react-query";
-
 import "./index.css";
 
 const queryClient = new QueryClient();
